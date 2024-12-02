1b9eaa13e0b9b545e8dda14b80c69a514c20bd22
Tuomas Hautamäki <tuomas.a.hautamaki@gmail.com>
2024-12-02 17:01:03 +0200

    Corrected Generate_datasets script. Added analysis script for methods in R.

--- Generate_datasets.R
+++ Generate_datasets.R
@@ -1,3 +1,99 @@
 library(huge)
 library(BDgraph)
 
+# Number of observations (sample size).
+n <- 120
+# Number of variables.
+ps <- c(100, 200)
+# Number of datasets.
+n_datasets <- 50
+
+for (p in ps) {
+  # Folder where to save datasets. Change "path\\to\\folder" part where you want to save files.
+  path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
+  #path <- paste0("path\\to\\folder\\p", p, "\\")
+  
+  # Initialise list where to save simulations.
+  sim_random <- list()
+  sim_bdgraph_sf <- list()
+  sim_huge_sf <- list()
+  sim_hubs <- list()
+  
+  for (i in 1:n_datasets) {
+    # Set seed so same datasets can be generated again.
+    seed <- 20241202 + i + p
+    set.seed(seed)
+    # Generate simulated dataset for random structure using bdgraph.sim function.
+    sim <- bdgraph.sim(p = p, n = n, graph = "random", size = p/2)
+    sim$omega <- sim$K
+    sim$theta <- sim$G
+    sim_random[[i]] <- sim
+    
+    # Set seed so same datasets can be generated again.
+    seed <- 20251202 + i + p
+    # Generate simulated dataset for random structure using bdgraph.sim function.
+    sim <- bdgraph.sim(p = p, n = n, graph = "scale-free")
+    sim$omega <- sim$K
+    sim$theta <- sim$G
+    sim_bdgraph_sf[[i]] <- sim
+    
+    # Set seed so same datasets can be generated again.
+    seed <- 20261202 + i + p
+    # Generate simulated dataset for random structure using bdgraph.sim function.
+    sim <- huge.generator(n = n, d = p, graph = "scale-free")
+    sim_huge_sf[[i]] <- sim
+    
+    # Set seed so same datasets can be generated again.
+    seed <- 20271202 + i + p
+    # Generate simulated dataset for random structure using bdgraph.sim function.
+    sim <- huge.generator(n = n, d = p, graph = "hub")
+    sim_hubs[[i]] <- sim
+  }
+  save(sim_random, file = paste0(path, "bdgraph_random_", p, ".Rda"))
+  save(sim_bdgraph_sf, file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+  save(sim_huge_sf, file = paste0(path, "huge_scale-free_", p, ".Rda"))
+  save(sim_hubs, file = paste0(path, "huge_hubs_", p, ".Rda"))
+}
+
+# Save necessary data also as csv-format for Matlab use.
+for (p in ps) {
+  # Path to folder where datasets are saved. Change "path\\to\\folder" part where you want to save and
+  # load files.
+  path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
+  #path <- paste0("path\\to\\folder\\p", p, "\\")
+  
+  # Load datasets.
+  load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
+  load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+  load(file = paste0(path, "huge_scale-free_", p, ".Rda"))
+  load(file = paste0(path, "huge_hubs_", p, ".Rda"))
+  for (i in 1:n_datasets) {
+    # Datasets with random structure from BDgraph.
+    sim <- sim_random[[i]]
+    write.csv(sim$data, paste0(path, "random\\random_data_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$sigma, paste0(path, "random\\random_sigma_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$omega, paste0(path, "random\\random_omega_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$theta, paste0(path, "random\\random_theta_nro_", i, ".csv"), row.names = FALSE)
+    
+    # Datasets with scale-free structure from BDgraph.
+    sim <- sim_bdgraph_sf[[i]]
+    write.csv(sim$data, paste0(path, "bdgraph_scale-free\\bdgraph_sf_data_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$sigma, paste0(path, "bdgraph_scale-free\\bdgraph_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$omega, paste0(path, "bdgraph_scale-free\\bdgraph_sf_omega_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$theta, paste0(path, "bdgraph_scale-free\\bdgraph_sf_theta_nro_", i, ".csv"), row.names = FALSE)
+    
+    # Datasets with scale-free structure from huge.
+    sim <- sim_huge_sf[[i]]
+    write.csv(sim$data, paste0(path, "huge_scale-free\\huge_sf_data_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$sigma, paste0(path, "huge_scale-free\\huge_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$omega, paste0(path, "huge_scale-free\\huge_sf_omega_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(as.matrix(sim$theta), paste0(path, "huge_scale-free\\huge_sf_theta_nro_", i, ".csv"), row.names = FALSE)
+    
+    # Datasets with hubs structure from huge.
+    sim <- sim_hubs[[i]]
+    write.csv(sim$data, paste0(path, "hubs\\hubs_data_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$sigma, paste0(path, "hubs\\hubs_sigma_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(sim$omega, paste0(path, "hubs\\hubs_omega_nro_", i, ".csv"), row.names = FALSE)
+    write.csv(as.matrix(sim$theta), paste0(path, "hubs\\hubs_theta_nro_", i, ".csv"), row.names = FALSE)
+  }
+}
---
+++ Run_analyses.R
@@ -0,0 +1,130 @@
+if(!require("GHSGEM", quietly = TRUE)) {
+  devtools::install_github("THautamaki/GHSGEM")
+}
+if(!require("beam", quietly = TRUE)) {
+  devtools::install_version("beam", version = "2.0.2")
+}
+
+library(GHSGEM)
+library(doParallel)
+library(beam)
+
+run_analysis <- function(simulations, method = "GEM") {
+  n_datasets <- length(simulations)
+  cores <- detectCores(logical = FALSE)
+  if (tolower(method) == "gem") {
+    packages <- "GHSGEM"
+  }
+  else if (tolower(method) == "glasso") {
+    packages <- c("huge", "GHSGEM")
+  }
+  else if (tolower(method) == "beam") {
+    packages <- c("beam", "GHSGEM")
+  }
+  else {
+    stop("Wrong method!")
+  }
+  start <- Sys.time()
+  cl <- makeCluster(cores)
+  registerDoParallel(cl)
+  results <- foreach (i = 1:n_datasets, .combine = "rbind", .packages = packages) %dopar% {
+    sim <- simulations[[i]]
+    if (tolower(method) == "gem") {
+      map <- GHS_MAP_estimate(sim$data, verbose = 0)
+      theta_est <- map$Theta
+      sigma_est <- map$Sigma_est
+      omega_est <- map$Omega_est
+      time <- map$tot_time
+    }
+    else if (tolower(method) == "glasso") {
+      start_glasso <- Sys.time()
+      glasso_est <- huge(sim$data, nlambda = 50, method = "glasso")
+      stars_select <- huge.select(glasso_est, criterion = "stars")
+      time <- as.numeric(difftime(Sys.time(), start_glasso, units = "secs"))
+      theta_est <- stars_select$refit
+      sigma_est <- stars_select$opt.cov
+      omega_est <- stars_select$opt.icov
+    }
+    else if (tolower(method) == "beam") {
+      beam_start <- Sys.time()
+      beam_est <- beam(sim$data)
+      beam_select <- beam.select(beam_est)
+      time <- as.numeric(difftime(Sys.time(), beam_start, units = "secs"))
+      theta_est <- igraph::as_adjacency_matrix(ugraph(beam_select), names = FALSE)
+    }
+    edge_count <- sum(theta_est) / 2
+    cm <- conf_matrix(sim$theta, theta_est)
+    sl_omega <- NA
+    sl_sigma <- NA
+    F_norm_sigma <- NA
+    F_norm_omega <- NA
+    if (tolower(method) != "beam") {
+      sl_omega <- stein_loss(sim$omega, omega_est)
+      sl_sigma <- stein_loss(sim$sigma, sigma_est)
+      F_norm_sigma <- norm(sim$sigma - sigma_est, type = "f")
+      F_norm_omega <- norm(sim$omega - omega_est, type = "f")
+    }
+    score <- cbind(i, calculate_scores(cm), edge_count, sl_sigma, sl_omega, F_norm_sigma,
+                   F_norm_omega, time)
+    score
+  }
+  stopCluster(cl)
+  stopImplicitCluster()
+  total_time <- Sys.time() - start
+  return(list(results = results, total_time = total_time))
+}
+
+ps <- c(100, 200)
+
+# Analysis for the datasets with 100 variables.
+
+p <- 100
+path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
+
+load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
+load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+load(file = paste0(path, "huge_scale-free_", p, ".Rda"))
+load(file = paste0(path, "huge_hubs_", p, ".Rda"))
+
+random_p100_results <- run_analysis(sim_random)
+bdgraph_sf_p100_results <- run_analysis(sim_bdgraph_sf)
+huge_sf_p100_results <- run_analysis(sim_huge_sf)
+hubs_p100_results <- run_analysis(sim_hubs)
+
+random_p100_beam_results <- run_analysis(sim_random, "beam")
+
+scores <- c("MCC", "F1", "TPR", "FPR", "FDR", "sl_omega", "F_norm_omega", "time")
+
+round(rbind(colMeans(random_p100_results$results[, scores]),
+            colMeans(bdgraph_sf_p100_results$results[, scores]),
+            colMeans(huge_sf_p100_results$results[, scores]),
+            colMeans(hubs_p100_results$results[, scores])), 4)
+
+round(colMeans(random_p100_beam_results$results[, scores]), 4)
+
+# Analysis for the datasets with 200 variables.
+
+p <- 200
+path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
+
+load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
+load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+load(file = paste0(path, "huge_scale-free_", p, ".Rda"))
+load(file = paste0(path, "huge_hubs_", p, ".Rda"))
+
+n_datasets <- length(sim_random)
+
+random_p200_results <- run_analysis(sim_random)
+bdgraph_sf_p200_results <- run_analysis(sim_bdgraph_sf)
+huge_sf_p200_results <- run_analysis(sim_huge_sf)
+hubs_p200_results <- run_analysis(sim_hubs)
+
+round(rbind(colMeans(random_p200_results$results[, scores]),
+            colMeans(bdgraph_sf_p200_results$results[, scores]),
+            colMeans(huge_sf_p200_results$results[, scores]),
+            colMeans(hubs_p200_results$results[, scores])), 4)
+
+
+random_p200_beam_results <- run_analysis(sim_random, "beam")
+
+round(colMeans(random_p200_beam_results$results[, scores]), 4)
