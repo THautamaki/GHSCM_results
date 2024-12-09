c7a39d8c9cab5412193744351f505ef5e7ceea41
Tuomas Hautamäki <tuomas.a.hautamaki@gmail.com>
2024-12-09 17:56:39 +0200

    Update both scripts. Implemented Pulsar for lambda tuning for glasso.

--- Generate_datasets.R
+++ Generate_datasets.R
@@ -8,10 +8,11 @@ ps <- c(100, 200)
 # Number of datasets.
 n_datasets <- 50
 
+save_datasets <- FALSE
+
 for (p in ps) {
   # Folder where to save datasets. Change "path\\to\\folder" part where you want to save files.
-  path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
-  #path <- paste0("path\\to\\folder\\p", p, "\\")
+  path <- paste0("path\\to\\folder\\p", p, "\\")
   
   # Initialise list where to save simulations.
   sim_random <- list()
@@ -49,51 +50,55 @@ for (p in ps) {
     sim <- huge.generator(n = n, d = p, graph = "hub")
     sim_hubs[[i]] <- sim
   }
-  save(sim_random, file = paste0(path, "bdgraph_random_", p, ".Rda"))
-  save(sim_bdgraph_sf, file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
-  save(sim_huge_sf, file = paste0(path, "huge_scale-free_", p, ".Rda"))
-  save(sim_hubs, file = paste0(path, "huge_hubs_", p, ".Rda"))
+  if (save_datasets) {
+    save(sim_random, file = paste0(path, "bdgraph_random_", p, ".Rda"))
+    save(sim_bdgraph_sf, file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+    save(sim_huge_sf, file = paste0(path, "huge_scale-free_", p, ".Rda"))
+    save(sim_hubs, file = paste0(path, "huge_hubs_", p, ".Rda"))
+  }
 }
 
 # Save necessary data also as csv-format for Matlab use.
-for (p in ps) {
-  # Path to folder where datasets are saved. Change "path\\to\\folder" part where you want to save and
-  # load files.
-  path <- paste0("C:\\Users\\thautama\\OneDrive - Oulun yliopisto\\Documents\\data\\Artikkeli\\p", p, "\\")
-  #path <- paste0("path\\to\\folder\\p", p, "\\")
-  
-  # Load datasets.
-  load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
-  load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
-  load(file = paste0(path, "huge_scale-free_", p, ".Rda"))
-  load(file = paste0(path, "huge_hubs_", p, ".Rda"))
-  for (i in 1:n_datasets) {
-    # Datasets with random structure from BDgraph.
-    sim <- sim_random[[i]]
-    write.csv(sim$data, paste0(path, "random\\random_data_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$sigma, paste0(path, "random\\random_sigma_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$omega, paste0(path, "random\\random_omega_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$theta, paste0(path, "random\\random_theta_nro_", i, ".csv"), row.names = FALSE)
+if (save_datasets) {
+  for (p in ps) {
+    # Path to folder where datasets are saved. Change "path\\to\\folder" part where you want to save and
+    # load files.
+    path <- paste0("path\\to\\folder\\p", p, "\\")
     
-    # Datasets with scale-free structure from BDgraph.
-    sim <- sim_bdgraph_sf[[i]]
-    write.csv(sim$data, paste0(path, "bdgraph_scale-free\\bdgraph_sf_data_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$sigma, paste0(path, "bdgraph_scale-free\\bdgraph_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$omega, paste0(path, "bdgraph_scale-free\\bdgraph_sf_omega_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$theta, paste0(path, "bdgraph_scale-free\\bdgraph_sf_theta_nro_", i, ".csv"), row.names = FALSE)
-    
-    # Datasets with scale-free structure from huge.
-    sim <- sim_huge_sf[[i]]
-    write.csv(sim$data, paste0(path, "huge_scale-free\\huge_sf_data_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$sigma, paste0(path, "huge_scale-free\\huge_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$omega, paste0(path, "huge_scale-free\\huge_sf_omega_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(as.matrix(sim$theta), paste0(path, "huge_scale-free\\huge_sf_theta_nro_", i, ".csv"), row.names = FALSE)
-    
-    # Datasets with hubs structure from huge.
-    sim <- sim_hubs[[i]]
-    write.csv(sim$data, paste0(path, "hubs\\hubs_data_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$sigma, paste0(path, "hubs\\hubs_sigma_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(sim$omega, paste0(path, "hubs\\hubs_omega_nro_", i, ".csv"), row.names = FALSE)
-    write.csv(as.matrix(sim$theta), paste0(path, "hubs\\hubs_theta_nro_", i, ".csv"), row.names = FALSE)
+    # Load datasets.
+    load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
+    load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
+    load(file = paste0(path, "huge_scale-free_", p, ".Rda"))
+    load(file = paste0(path, "huge_hubs_", p, ".Rda"))
+    for (i in 1:n_datasets) {
+      # Datasets with random structure from BDgraph.
+      sim <- sim_random[[i]]
+      write.csv(sim$data, paste0(path, "random\\random_data_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$sigma, paste0(path, "random\\random_sigma_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$omega, paste0(path, "random\\random_omega_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$theta, paste0(path, "random\\random_theta_nro_", i, ".csv"), row.names = FALSE)
+      
+      # Datasets with scale-free structure from BDgraph.
+      sim <- sim_bdgraph_sf[[i]]
+      write.csv(sim$data, paste0(path, "bdgraph_scale-free\\bdgraph_sf_data_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$sigma, paste0(path, "bdgraph_scale-free\\bdgraph_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$omega, paste0(path, "bdgraph_scale-free\\bdgraph_sf_omega_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$theta, paste0(path, "bdgraph_scale-free\\bdgraph_sf_theta_nro_", i, ".csv"), row.names = FALSE)
+      
+      # Datasets with scale-free structure from huge.
+      sim <- sim_huge_sf[[i]]
+      write.csv(sim$data, paste0(path, "huge_scale-free\\huge_sf_data_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$sigma, paste0(path, "huge_scale-free\\huge_sf_sigma_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$omega, paste0(path, "huge_scale-free\\huge_sf_omega_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(as.matrix(sim$theta), paste0(path, "huge_scale-free\\huge_sf_theta_nro_", i, ".csv"), row.names = FALSE)
+      
+      # Datasets with hubs structure from huge.
+      sim <- sim_hubs[[i]]
+      write.csv(sim$data, paste0(path, "hubs\\hubs_data_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$sigma, paste0(path, "hubs\\hubs_sigma_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(sim$omega, paste0(path, "hubs\\hubs_omega_nro_", i, ".csv"), row.names = FALSE)
+      write.csv(as.matrix(sim$theta), paste0(path, "hubs\\hubs_theta_nro_", i, ".csv"), row.names = FALSE)
+    }
   }
 }
+
--- Run_analyses.R
+++ Run_analyses.R
@@ -9,6 +9,7 @@ library(GHSGEM)
 library(beam)
 library(huge)
 library(doParallel)
+library(pulsar)
 
 run_analysis <- function(simulations, method = "GEM") {
   n_datasets <- length(simulations)
@@ -22,13 +23,17 @@ run_analysis <- function(simulations, method = "GEM") {
   else if (tolower(method) == "beam") {
     packages <- c("beam", "GHSGEM")
   }
+  else if (tolower(method) == "pulsar") {
+    packages <- c("pulsar", "huge", "GHSGEM")
+  }
   else {
     stop("Wrong method!")
   }
-  start <- Sys.time()
   cl <- makeCluster(cores)
   registerDoParallel(cl)
-  results <- foreach (i = 1:n_datasets, .combine = "rbind", .packages = packages) %dopar% {
+  start <- Sys.time()
+  results <- foreach (i = 1:n_datasets, .combine = "rbind", .packages = packages,
+                      .verbose = FALSE) %dopar% {
     sim <- simulations[[i]]
     if (tolower(method) == "gem") {
       map <- GHS_MAP_estimate(sim$data, verbose = 0)
@@ -52,6 +57,19 @@ run_analysis <- function(simulations, method = "GEM") {
       time <- as.numeric(difftime(Sys.time(), beam_start, units = "secs"))
       theta_est <- igraph::as_adjacency_matrix(ugraph(beam_select), names = FALSE)
     }
+    else if (tolower(method) == "pulsar") {
+      pulsar_start <- Sys.time()
+      #lmax <- getMaxCov(cov(sim$data))
+      #lams <- getLamPath(lmax, lmax * 0.05, len = 50)
+      lams <- huge(sim$data, nlambda = 50, method = "glasso")$lambda
+      bdgraphargs <- list(lambda = lams, method = "glasso", verbose = FALSE)
+      out.p <- pulsar(sim$data, fun = huge, fargs = bdgraphargs, rep.num = 20, criterion = 'stars',
+                      lb.stars = TRUE, ub.stars = TRUE)
+      fit.p <- refit(out.p)
+      time <- as.numeric(difftime(Sys.time(), pulsar_start, units = "secs"))
+      theta_est <- as.matrix(fit.p$refit$stars)
+      diag(theta_est) <- 0
+    }
     edge_count <- sum(theta_est) / 2
     cm <- conf_matrix(sim$theta, theta_est)
     sl_omega <- NA
@@ -72,18 +90,17 @@ run_analysis <- function(simulations, method = "GEM") {
                    F_norm_omega, time)
     score
   }
+  total_time <- Sys.time() - start
   stopCluster(cl)
   stopImplicitCluster()
-  total_time <- Sys.time() - start
   return(list(results = results, total_time = total_time))
 }
 
-ps <- c(100, 200)
-
 # Analysis for the datasets with 100 variables.
-
 p <- 100
-path <- paste0("C:\\Users\\tume8\\Documents\\data\\Artikkeli\\p", p, "\\")
+
+# Change "path\\to\\data" part where you have stored datasets.
+path <- paste0("path\\to\\data\\p", p, "\\")
 
 load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
 load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
@@ -105,8 +122,13 @@ bdgraph_sf_p100_glasso_results <- run_analysis(sim_bdgraph_sf, "glasso")
 huge_sf_p100_glasso_results <- run_analysis(sim_huge_sf, "glasso")
 hubs_p100_glasso_results <- run_analysis(sim_hubs, "glasso")
 
+bdgraph_sf_p100_pulsar_results <- run_analysis(sim_bdgraph_sf, "pulsar")
+huge_sf_p100_pulsar_results <- run_analysis(sim_huge_sf, "pulsar")
+
+# Select scores which wanted to print.
 scores <- c("MCC", "F1", "TPR", "FPR", "FDR", "sl_omega", "F_norm_omega", "time")
 
+# Print scores.
 round(rbind(colMeans(random_p100_results$results[, scores]),
             colMeans(random_p100_glasso_results$results[, scores]),
             colMeans(random_p100_beam_results$results[, scores])), 4)
@@ -123,25 +145,28 @@ round(rbind(colMeans(hubs_p100_results$results[, scores]),
             colMeans(hubs_p100_glasso_results$results[, scores]),
             colMeans(hubs_p100_beam_results$results[, scores])), 4)
 
-save(random_p100_results, file = "random_p100_GEM_results.Rda")
-save(bdgraph_sf_p100_results, file = "bdgraph_sf_p100_GEM_results.Rda")
-save(huge_sf_p100_results, file = "huge_sf_p100_GEM_results.Rda")
-save(hubs_p100_results, file = "hubs_p100_GEM_results.Rda")
+# Save results into "Results_files" folder which is in the working directory.
+save(random_p100_results, file = "Results_files/random_p100_GEM_results.Rda")
+save(bdgraph_sf_p100_results, file = "Results_files/bdgraph_sf_p100_GEM_results.Rda")
+save(huge_sf_p100_results, file = "Results_files/huge_sf_p100_GEM_results.Rda")
+save(hubs_p100_results, file = "Results_files/hubs_p100_GEM_results.Rda")
 
-save(random_p100_glasso_results, file = "random_p100_glasso_results.Rda")
-save(bdgraph_sf_p100_glasso_results, file = "bdgraph_sf_p100_glasso_results.Rda")
-save(huge_sf_p100_glasso_results, file = "huge_sf_p100_glasso_results.Rda")
-save(hubs_p100_glasso_results, file = "hubs_p100_glasso_results.Rda")
+save(random_p100_glasso_results, file = "Results_files/random_p100_glasso_results.Rda")
+save(bdgraph_sf_p100_glasso_results, file = "Results_files/bdgraph_sf_p100_glasso_results.Rda")
+save(huge_sf_p100_glasso_results, file = "Results_files/huge_sf_p100_glasso_results.Rda")
+save(hubs_p100_glasso_results, file = "Results_files/hubs_p100_glasso_results.Rda")
 
-save(random_p100_beam_results, file = "random_p100_beam_results.Rda")
-save(bdgraph_sf_p100_beam_results, file = "bdgraph_sf_p100_beam_results.Rda")
-save(huge_sf_p100_beam_results, file = "huge_sf_p100_beam_results.Rda")
-save(hubs_p100_beam_results, file = "hubs_p100_beam_results.Rda")
+save(random_p100_beam_results, file = "Results_files/random_p100_beam_results.Rda")
+save(bdgraph_sf_p100_beam_results, file = "Results_files/bdgraph_sf_p100_beam_results.Rda")
+save(huge_sf_p100_beam_results, file = "Results_files/huge_sf_p100_beam_results.Rda")
+save(hubs_p100_beam_results, file = "Results_files/hubs_p100_beam_results.Rda")
+
 
 # Analysis for the datasets with 200 variables.
-
 p <- 200
-path <- paste0("C:\\Users\\tume8\\Documents\\data\\Artikkeli\\p", p, "\\")
+
+# Change "path\\to\\data" part where you have stored datasets.
+path <- paste0("path\\to\\data\\p", p, "\\")
 
 load(file = paste0(path, "bdgraph_random_", p, ".Rda"))
 load(file = paste0(path, "bdgraph_scale-free_", p, ".Rda"))
@@ -165,6 +190,10 @@ bdgraph_sf_p200_glasso_results <- run_analysis(sim_bdgraph_sf, "glasso")
 huge_sf_p200_glasso_results <- run_analysis(sim_huge_sf, "glasso")
 hubs_p200_glasso_results <- run_analysis(sim_hubs, "glasso")
 
+# Select scores which wanted to print.
+scores <- c("MCC", "F1", "TPR", "FPR", "FDR", "sl_omega", "F_norm_omega", "time")
+
+# Print scores.
 round(rbind(colMeans(random_p200_results$results[, scores]),
             colMeans(random_p200_glasso_results$results[, scores]),
             colMeans(random_p200_beam_results$results[, scores])), 4)
@@ -181,49 +210,184 @@ round(rbind(colMeans(hubs_p200_results$results[, scores]),
             colMeans(hubs_p200_glasso_results$results[, scores]),
             colMeans(hubs_p200_beam_results$results[, scores])), 4)
 
-save(random_p200_results, file = "random_p200_GEM_results.Rda")
-save(bdgraph_sf_p200_results, file = "bdgraph_sf_p200_GEM_results.Rda")
-save(huge_sf_p200_results, file = "huge_sf_p200_GEM_results.Rda")
-save(hubs_p200_results, file = "hubs_p200_GEM_results.Rda")
+save(random_p200_results, file = "Results_files/random_p200_GEM_results.Rda")
+save(bdgraph_sf_p200_results, file = "Results_files/bdgraph_sf_p200_GEM_results.Rda")
+save(huge_sf_p200_results, file = "Results_files/huge_sf_p200_GEM_results.Rda")
+save(hubs_p200_results, file = "Results_files/hubs_p200_GEM_results.Rda")
 
-save(random_p200_glasso_results, file = "random_p200_glasso_results.Rda")
-save(bdgraph_sf_p200_glasso_results, file = "bdgraph_sf_p200_glasso_results.Rda")
-save(huge_sf_p200_glasso_results, file = "huge_sf_p200_glasso_results.Rda")
-save(hubs_p200_glasso_results, file = "hubs_p200_glasso_results.Rda")
+save(random_p200_glasso_results, file = "Results_files/random_p200_glasso_results.Rda")
+save(bdgraph_sf_p200_glasso_results, file = "Results_files/bdgraph_sf_p200_glasso_results.Rda")
+save(huge_sf_p200_glasso_results, file = "Results_files/huge_sf_p200_glasso_results.Rda")
+save(hubs_p200_glasso_results, file = "Results_files/hubs_p200_glasso_results.Rda")
 
-save(random_p200_beam_results, file = "random_p200_beam_results.Rda")
-save(bdgraph_sf_p200_beam_results, file = "bdgraph_sf_p200_beam_results.Rda")
-save(huge_sf_p200_beam_results, file = "huge_sf_p200_beam_results.Rda")
-save(hubs_p200_beam_results, file = "hubs_p200_beam_results.Rda")
+save(random_p200_beam_results, file = "Results_files/random_p200_beam_results.Rda")
+save(bdgraph_sf_p200_beam_results, file = "Results_files/bdgraph_sf_p200_beam_results.Rda")
+save(huge_sf_p200_beam_results, file = "Results_files/huge_sf_p200_beam_results.Rda")
+save(hubs_p200_beam_results, file = "Results_files/hubs_p200_beam_results.Rda")
 
 
-########
+#########
+# Recalculate scores if analyses are already run and saved.
+# Load results files.
 
-n_datasets <- length(sim_random)
-cores <- detectCores(logical = FALSE)
+load(file = "Results_files/random_p100_GEM_results.Rda")
+load(file = "Results_files/bdgraph_sf_p100_GEM_results.Rda")
+load(file = "Results_files/huge_sf_p100_GEM_results.Rda")
+load(file = "Results_files/hubs_p100_GEM_results.Rda")
 
-start <- Sys.time()
-cl <- makeCluster(cores)
-registerDoParallel(cl)
-results <- foreach (i = 1:n_datasets, .combine = "rbind", .packages = "GHSGEM") %dopar% {
-  sim <- sim_random[[i]]
-  map <- GHS_MAP_estimate(sim$data, verbose = 0)
-  theta_est <- map$Theta
-  sigma_est <- map$Sigma_est
-  omega_est <- map$Omega_est
-  time <- map$tot_time
+load(file = "Results_files/random_p100_glasso_results.Rda")
+load(file = "Results_files/bdgraph_sf_p100_glasso_results.Rda")
+load(file = "Results_files/huge_sf_p100_glasso_results.Rda")
+load(file = "Results_files/hubs_p100_glasso_results.Rda")
 
-  edge_count <- sum(theta_est) / 2
-  cm <- conf_matrix(sim$theta, theta_est)
-  sl_omega <- stein_loss(sim$omega, omega_est)
-  sl_sigma <- stein_loss(sim$sigma, sigma_est)
-  F_norm_sigma <- norm(sim$sigma - sigma_est, type = "f")
-  F_norm_omega <- norm(sim$omega - omega_est, type = "f")
-  score <- cbind(i, calculate_scores(cm), edge_count, sl_sigma, sl_omega, F_norm_sigma,
-                 F_norm_omega, time)
-  score
-}
-stopCluster(cl)
-stopImplicitCluster()
-Sys.time() - start
+load(file = "Results_files/random_p100_beam_results.Rda")
+load(file = "Results_files/bdgraph_sf_p100_beam_results.Rda")
+load(file = "Results_files/huge_sf_p100_beam_results.Rda")
+load(file = "Results_files/hubs_p100_beam_results.Rda")
+
+load(file = "Results_files/random_p200_GEM_results.Rda")
+load(file = "Results_files/bdgraph_sf_p200_GEM_results.Rda")
+load(file = "Results_files/huge_sf_p200_GEM_results.Rda")
+load(file = "Results_files/hubs_p200_GEM_results.Rda")
+
+load(file = "Results_files/random_p200_glasso_results.Rda")
+load(file = "Results_files/bdgraph_sf_p200_glasso_results.Rda")
+load(file = "Results_files/huge_sf_p200_glasso_results.Rda")
+load(file = "Results_files/hubs_p200_glasso_results.Rda")
+
+load(file = "Results_files/random_p200_beam_results.Rda")
+load(file = "Results_files/bdgraph_sf_p200_beam_results.Rda")
+load(file = "Results_files/huge_sf_p200_beam_results.Rda")
+load(file = "Results_files/hubs_p200_beam_results.Rda")
+
+scores <- c("MCC", "F1", "TPR", "FPR", "FDR", "sl_omega", "F_norm_omega", "time")
+
+# Means of the scores:
+# p = 100.
+# Random network.
+round(rbind(colMeans(random_p100_results$results[, scores]),
+            colMeans(random_p100_glasso_results$results[, scores]),
+            colMeans(random_p100_beam_results$results[, scores])), 4)
+
+# Scale-free network (BDgraph).
+round(rbind(colMeans(bdgraph_sf_p100_results$results[, scores]),
+            colMeans(bdgraph_sf_p100_glasso_results$results[, scores]),
+            colMeans(bdgraph_sf_p100_beam_results$results[, scores])), 4)
+
+# Scale-free network (huge).
+round(rbind(colMeans(huge_sf_p100_results$results[, scores]),
+            colMeans(huge_sf_p100_glasso_results$results[, scores]),
+            colMeans(huge_sf_p100_beam_results$results[, scores])), 4)
+
+# Hubs network.
+round(rbind(colMeans(hubs_p100_results$results[, scores]),
+            colMeans(hubs_p100_glasso_results$results[, scores]),
+            colMeans(hubs_p100_beam_results$results[, scores])), 4)
+
+# p = 200.
+# Random network.
+round(rbind(colMeans(random_p200_results$results[, scores]),
+            colMeans(random_p200_glasso_results$results[, scores]),
+            colMeans(random_p200_beam_results$results[, scores])), 4)
+
+# Scale-free network (BDgraph).
+round(rbind(colMeans(bdgraph_sf_p200_results$results[, scores]),
+            colMeans(bdgraph_sf_p200_glasso_results$results[, scores]),
+            colMeans(bdgraph_sf_p200_beam_results$results[, scores])), 4)
+
+# Scale-free network (huge).
+round(rbind(colMeans(huge_sf_p200_results$results[, scores]),
+            colMeans(huge_sf_p200_glasso_results$results[, scores]),
+            colMeans(huge_sf_p200_beam_results$results[, scores])), 4)
+
+# Hubs network.
+round(rbind(colMeans(hubs_p200_results$results[, scores]),
+            colMeans(hubs_p200_glasso_results$results[, scores]),
+            colMeans(hubs_p200_beam_results$results[, scores])), 4)
+
+# SD:s of the results:
+# p = 100.
+# Random network.
+round(rbind(apply(random_p100_results$results[, scores], 2, sd),
+            apply(random_p100_glasso_results$results[, scores], 2, sd),
+            apply(random_p100_beam_results$results[, scores], 2, sd)), 4)
+
+# Scale-free network (BDgraph).
+round(rbind(apply(bdgraph_sf_p100_results$results[, scores], 2, sd),
+            apply(bdgraph_sf_p100_glasso_results$results[, scores], 2, sd),
+            apply(bdgraph_sf_p100_beam_results$results[, scores], 2, sd)), 4)
+
+# Scale-free network (huge).
+round(rbind(apply(huge_sf_p100_results$results[, scores], 2, sd),
+            apply(huge_sf_p100_glasso_results$results[, scores], 2, sd),
+            apply(huge_sf_p100_beam_results$results[, scores], 2, sd)), 4)
+
+# Hubs network.
+round(rbind(apply(hubs_p100_results$results[, scores], 2, sd),
+            apply(hubs_p100_glasso_results$results[, scores], 2, sd),
+            apply(hubs_p100_beam_results$results[, scores], 2, sd)), 4)
+
+# p = 200.
+# Random network.
+round(rbind(apply(random_p200_results$results[, scores], 2, sd),
+            apply(random_p200_glasso_results$results[, scores], 2, sd),
+            apply(random_p200_beam_results$results[, scores], 2, sd)), 4)
+
+# Scale-free network (BDgraph).
+round(rbind(apply(bdgraph_sf_p200_results$results[, scores], 2, sd),
+            apply(bdgraph_sf_p200_glasso_results$results[, scores], 2, sd),
+            apply(bdgraph_sf_p200_beam_results$results[, scores], 2, sd)), 4)
+
+# Scale-free network (huge).
+round(rbind(apply(huge_sf_p200_results$results[, scores], 2, sd),
+            apply(huge_sf_p200_glasso_results$results[, scores], 2, sd),
+            apply(huge_sf_p200_beam_results$results[, scores], 2, sd)), 4)
+
+# Hubs network.
+round(rbind(apply(hubs_p200_results$results[, scores], 2, sd),
+            apply(hubs_p200_glasso_results$results[, scores], 2, sd),
+            apply(hubs_p200_beam_results$results[, scores], 2, sd)), 4)
+
+# Total running times:
+# p = 100.
+# Random network.
+random_p100_results$total_time
+random_p100_glasso_results$total_time
+random_p100_beam_results$total_time
+
+# Scale-free network (BDgraph).
+bdgraph_sf_p100_results$total_time
+as.double(bdgraph_sf_p100_glasso_results$total_time, units = "secs")
+bdgraph_sf_p100_beam_results$total_time
+
+# Scale-free network (huge).
+huge_sf_p100_results$total_time
+huge_sf_p100_glasso_results$total_time
+huge_sf_p100_beam_results$total_time
+
+# Hubs network.
+hubs_p100_results$total_time
+hubs_p100_glasso_results$total_time
+hubs_p100_beam_results$total_time
+
+# p = 200.
+# Random network.
+random_p200_results$total_time
+as.double(random_p200_glasso_results$total_time, units = "secs")
+random_p200_beam_results$total_time
+
+# Scale-free network (BDgraph).
+as.double(bdgraph_sf_p200_results$total_time, units = "secs")
+as.double(bdgraph_sf_p200_glasso_results$total_time, units = "secs")
+bdgraph_sf_p200_beam_results$total_time
+
+# Scale-free network (huge).
+as.double(huge_sf_p200_results$total_time, units = "secs")
+as.double(huge_sf_p200_glasso_results$total_time, units = "secs")
+huge_sf_p200_beam_results$total_time
+
+# Hubs network.
+as.double(hubs_p200_results$total_time, units = "secs")
+as.double(hubs_p200_glasso_results$total_time, units = "secs")
+hubs_p200_beam_results$total_time
 
